5781a3f9db6db59fa38bd364a0dbb82220b0d905
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2025-03-24 09:47:25 +0700

    feat: update file name

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type SheetData struct {
@@ -14,10 +16,22 @@ type SheetData struct {
 }
 
 func main() {
-	fmt.Println("Gmail and INJ Address Scanner - Reading from registered.json")
+	fmt.Println("Gmail and INJ Address Scanner - Reading")
+
+	// Get filename from environment variable
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Error loading .env file: %v\n", err)
+		os.Exit(1)
+	}
+
+	filename := os.Getenv("REGISTERED_FILE")
+	if filename == "" {
+		fmt.Println("REGISTERED_FILE not set in .env")
+		os.Exit(1)
+	}
 
 	// Read the JSON file
-	jsonFile, err := ioutil.ReadFile("registered.json")
+	jsonFile, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
 		os.Exit(1)
