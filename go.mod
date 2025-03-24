f8476e95820dea90417fb4b8504dd6468c39cae2
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2025-03-24 17:01:12 +0700

    feat: add check nft

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module injscan-checking-address
 
 go 1.23.4
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/tidwall/pretty v1.2.1
+)
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
+
+var ListNFT = []string{"Injective Quants", "The Ninjas"}
 
 type SheetData struct {
 	Range          string     `json:"range"`
@@ -15,7 +18,63 @@ type SheetData struct {
 	Values         [][]string `json:"values"`
 }
 
+// func checkBalance(injAddress string) {
+
+// }
+
+// func checkTx(injAddress string, apiKey string) bool {
+// 	return false
+// }
+
+func checkNft(injAddress string, apiKey string) bool {
+	// Make API request to check INJ address
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.talis.art/tokens/"+injAddress+"?offset=0&limit=100", nil)
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		return false
+	}
+
+	req.Header.Add("x-api-key", apiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error making request: %v\n", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Read and parse response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response: %v\n", err)
+		return false
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Printf("Error parsing response: %v\n", err)
+		return false
+	}
+
+	if result["tokens"] == nil {
+		return false
+	}
+
+	for _, token := range result["tokens"].([]interface{}) {
+		tokenMap := token.(map[string]interface{})
+		family := tokenMap["family"].(map[string]interface{})
+		for _, nft := range ListNFT {
+			if family["name"] == nft {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func main() {
+
 	fmt.Println("Gmail and INJ Address Scanner - Reading")
 
 	// Get filename from environment variable
@@ -27,6 +86,12 @@ func main() {
 	filename := os.Getenv("REGISTERED_FILE")
 	if filename == "" {
 		fmt.Println("REGISTERED_FILE not set in .env")
+		os.Exit(1)
+	}
+
+	apiKey := os.Getenv("TALIS_API_KEY")
+	if apiKey == "" {
+		fmt.Println("TALIS_API_KEY not set in .env")
 		os.Exit(1)
 	}
 
@@ -55,19 +120,74 @@ func main() {
 	fmt.Println("\nScanning registered addresses:")
 	fmt.Println("------------------------------------")
 
-	for i, row := range sheetData.Values[1:] {
+	// Create a file to store results
+	resultsFile := "results.json"
+	results := struct {
+		Addresses []string `json:"addresses"`
+	}{
+		Addresses: make([]string, 0),
+	}
+
+	// Remove existing file if it exists
+	if _, err := os.Stat(resultsFile); err == nil {
+		if err := os.Remove(resultsFile); err != nil {
+			fmt.Printf("Error removing existing results file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Create new results file
+	f, err := os.Create(resultsFile)
+	if err != nil {
+		fmt.Printf("Error creating results file: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// Extract just the addresses from data
+	for _, row := range sheetData.Values[1:] {
+		if len(row) >= 2 {
+			results.Addresses = append(results.Addresses, row[1])
+		}
+	}
+
+	var addresses []string
+
+	for _, row := range sheetData.Values[1:] {
 		if len(row) < 2 {
 			continue
 		}
 
-		email := row[0]
+		// email := row[0]
 		injAddress := row[1]
 
-		// Print validation results
-		fmt.Printf("Record #%d:\n", i+1)
-		fmt.Printf("  Email: %s \n", email)
-		fmt.Printf("  INJ:   %s \n", injAddress)
-		fmt.Println()
+		// check balance of injAddress
+
+		// check tx of injAddress
+		// tx := checkTx(injAddress, apiKey)
+		// if tx {
+		// 	fmt.Println("TX found - ", injAddress)
+		// 	addresses = append(addresses, injAddress)
+		// 	continue
+		// }
+
+		// check nft of injAddress
+		nft := checkNft(injAddress, apiKey)
+		if nft {
+			fmt.Println("NFT found - ", injAddress)
+			addresses = append(addresses, injAddress)
+		}
+
+		// fmt.Printf("\nAddress: %s\n", injAddress)
+		// fmt.Printf("Response:\n%s\n", pretty.Pretty(body))
+	}
+
+	// Write results to file
+	encoder := json.NewEncoder(f)
+	encoder.SetIndent("", "    ")
+	if err := encoder.Encode(addresses); err != nil {
+		fmt.Printf("Error writing results: %v\n", err)
+		os.Exit(1)
 	}
 
 	fmt.Println("\nSummary:")
